e398aa96f8da0d843ac19bf249ac1c8fa5d8d40e
KrajewskiPiotr <p.krajewski@interia.eu>
2017-04-12 10:36:19 +0200

    1-FOSUserBundle OK

---
+++ AdBoard/README.md
@@ -0,0 +1,4 @@
+AdBoard
+=======
+
+A Symfony project created on April 12, 2017, 9:40 am.
---
+++ AdBoard/src/AB/AdBoardBundle/ABAdBoardBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AB\AdBoardBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ABAdBoardBundle extends Bundle
+{
+}
